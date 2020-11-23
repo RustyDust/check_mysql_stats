47a858f22b2d05b2a5b99a892af6261423949df3
RustyDust <stefan@whocares.de>
2020-11-23 16:39:43 +0100

    Add initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# check_mysql_stats
+
+A simple tool to check some MySQL/MariaDB statistics gathered from information_schema.global_stats.
+It's not pretty but I wanted to play with Go a bit and while doing so create something I could use as well.
+
+## Build
+
+- Install Go
+- Clone the repostory
+- run `go build`
+
+## Parameters
+
+* `-h` - MySQL/MariaDB host to connect to (default: 127.0.0.1)
+* `-p` - the port MySQL/MariaDB listens on (default: 3306)
+* `-u` - the username used for connecting to MySQL/MariaDB
+* `-p` - the password to use when connecting
+* `-rwarn` - warning level for read requests (in ops/s, default: 250)
+* `-rcrit` - critical level for read requests (in ops/s, default: 500)
+* `-wwarn` - warning level for write requests (in ops/s, default: 50)
+* `-wcrit` - critical level for write requests (in ops/s, default: 100)
+* `-t` - timeout when connecting to MySQL/MariaDB (in s, default: 5)
+* `-v` - display version information
+* `-help` - display help/usage information
+
+## Output
+The plugin will output a string of the format
+
+`<state>: <reason> | <metrics>`
+
+### state
+can be on of the following:
+* `OK` - everything is fine
+* `WARNING`- at least one of either the read or the write ops warning levels has been reached
+* `CRITICAL`- at least on of either the read or the write ops warning level has been reached
+
+### reason
+Just a text describing the state of the MySQL/MaraiDB instance.
+
+### metrics
+Multiple valies separated by spaces giving some statistics about the MySQL/MariaDB instance:
+
+* `queries` - total number of queries since last restart
+* `selects` - total number of SELECT queries since last restart
+* `inserts` - total number of INSERT queries since last restart
+* `updates` - total number of UPDATE queries since last restart
+* `deletes` - total number of DELETE queries since last restart
+* `uptime` - instance uptime in seconds
+* `reads_per_second` - calculated number of read requests per second since last run (or last restart if first run)
+* `writes_per_second` - calculated number of write requests per second since last run (or last restart if first run)
+
+### Data Store
+For every host checked to tool will create a JSON file containing the measurements taken on the last run. The file is
+placed into the same directory where the tool resides with a filename of
+
+`<name of tool>.<host address as passed by icinga>.stats`
+
+These files can be safely deleted. In case they don't exist a new version will be created and the number of seconds since the last restart as reported by MySQL/MariaDB will be used to generate the Rps/Wps counters.
---
+++ main.go
@@ -0,0 +1,188 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// Stats : structure to hold our MySQL statistics values
+type Stats struct {
+	Queries int64 `json:"queries"`
+	Selects int64 `json:"selects"`
+	Inserts int64 `json:"inserts"`
+	Updates int64 `json:"updates"`
+	Deletes int64 `json:"deletes"`
+	Uptime  int64 `json:"uptime"`
+	Totals  int64 `json:"totals"`
+}
+
+func main() {
+	// get command line flags
+
+	var myPath string
+	var icingaOut string
+	var exitCode int
+
+	myVersion := "0.0.1"
+
+	hostAddress := flag.String("h", "127.0.0.1", "Host ")
+	hostPort := flag.String("p", "3306", "Target port")
+	userName := flag.String("u", "", "MySQL user")
+	passWord := flag.String("P", "", "MySQL password for user")
+	timeOut := flag.Int("t", 2, "MySQL connection timeout in seconds")
+	version := flag.Bool("v", false, "Display version information")
+	rWarn := flag.Int64("rwarn", 1000, "Warning level read operations")
+	rCrit := flag.Int64("rcrit", 1500, "Critical level read operations")
+	wWarn := flag.Int64("wwarn", 50, "Warning level write operations")
+	wCrit := flag.Int64("wcrit", 100, "Critical level write operations")
+
+	flag.Parse()
+
+	if *version {
+		fmt.Println(fmt.Sprintf("%s v%s", os.Args[0], myVersion))
+		os.Exit(0)
+	}
+
+	myPath, _ = os.Getwd()
+
+	// Get stats from last run
+	lastStats, _ := getOldStats(myPath + "/" + os.Args[0][2:] + "." + *hostAddress + ".stats")
+
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/?timeout=%ds", *userName, *passWord, *hostAddress, *hostPort, *timeOut))
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	var result string
+	var stats Stats
+
+	// Get total number of client side queries
+	// err = db.QueryRow("SHOW GLOBAL STATUS LIKE 'questions'").Scan(&stats.queries, dummy)
+	err = db.QueryRow("SELECT VARIABLE_VALUE FROM information_schema.global_status WHERE VARIABLE_NAME = 'QUESTIONS'").Scan(&result)
+	if err != nil {
+		panic(err.Error())
+	}
+	stats.Queries, err = strconv.ParseInt(result, 10, 64)
+
+	// Get total number of client side deletes
+	err = db.QueryRow("SELECT VARIABLE_VALUE FROM information_schema.global_status WHERE VARIABLE_NAME = 'COM_DELETE'").Scan(&result)
+	if err != nil {
+		panic(err.Error())
+	}
+	stats.Deletes, err = strconv.ParseInt(result, 10, 64)
+
+	// Get total number of client side inserts
+	err = db.QueryRow("SELECT VARIABLE_VALUE FROM information_schema.global_status WHERE VARIABLE_NAME = 'COM_INSERT'").Scan(&result)
+	if err != nil {
+		panic(err.Error())
+	}
+	stats.Inserts, err = strconv.ParseInt(result, 10, 64)
+
+	// Get total number of client side updates
+	err = db.QueryRow("SELECT VARIABLE_VALUE FROM information_schema.global_status WHERE VARIABLE_NAME = 'COM_UPDATE'").Scan(&result)
+	if err != nil {
+		panic(err.Error())
+	}
+	stats.Updates, err = strconv.ParseInt(result, 10, 64)
+
+	// Get total number of client side selects
+	err = db.QueryRow("SELECT VARIABLE_VALUE FROM information_schema.global_status WHERE VARIABLE_NAME = 'COM_SELECT'").Scan(&result)
+	if err != nil {
+		panic(err.Error())
+	}
+	stats.Selects, err = strconv.ParseInt(result, 10, 64)
+
+	// Get Uptime of current server in seconds
+	err = db.QueryRow("SELECT VARIABLE_VALUE FROM information_schema.global_status WHERE VARIABLE_NAME = 'UPTIME'").Scan(&result)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	stats.Uptime, err = strconv.ParseInt(result, 10, 64)
+	stats.Totals = stats.Selects + stats.Deletes + stats.Inserts + stats.Updates
+
+	// Check for MySQL restart - reset counters if there was
+	if lastStats.Uptime > stats.Uptime {
+		lastStats.Uptime = 0
+		lastStats.Deletes = 0
+		lastStats.Inserts = 0
+		lastStats.Queries = 0
+		lastStats.Selects = 0
+		lastStats.Totals = 0
+		lastStats.Updates = 0
+	}
+
+	// write the data to the SQLite store
+	err = writeNewStats(myPath+"/"+os.Args[0][2:]+"."+*hostAddress+".stats", stats)
+
+	diffTime := stats.Uptime - lastStats.Uptime
+
+	// curOps := (stats.Totals - lastStats.Totals) / diffTime
+	curRps := (stats.Selects - lastStats.Selects) / diffTime
+	curWps := (stats.Deletes + stats.Updates + stats.Inserts - lastStats.Deletes - lastStats.Updates - lastStats.Inserts) / diffTime
+
+	icingaOut = "OK: Normal level of reads and writes"
+	exitCode = 0
+	if curRps > *rCrit {
+		icingaOut = "CRITICAL: Reads above critical level (" + ")"
+		exitCode = 2
+	} else {
+		if curRps > *rWarn {
+			icingaOut = "WARNING: Reads above warning level"
+			exitCode = 1
+		}
+	}
+
+	if curWps > *wCrit {
+		icingaOut = "CRITICAL: Writes above critical level"
+		exitCode = 2
+	} else {
+		if curWps > *wWarn {
+			icingaOut = "WARNING: Writes above warning level"
+			exitCode = 1
+		}
+	}
+
+	icingaOut += " | "
+	icingaOut += fmt.Sprintf("'queries'=%d ", stats.Queries)
+	icingaOut += fmt.Sprintf("'selects'=%d ", stats.Selects)
+	icingaOut += fmt.Sprintf("'inserts'=%d ", stats.Inserts)
+	icingaOut += fmt.Sprintf("'updates'=%d ", stats.Updates)
+	icingaOut += fmt.Sprintf("'deletes'=%d ", stats.Deletes)
+	icingaOut += fmt.Sprintf("'uptime'=%d ", stats.Uptime)
+	icingaOut += fmt.Sprintf("'reads_per_second'=%d ", curRps)
+	icingaOut += fmt.Sprintf("'writes_per_second'=%d ", curWps)
+
+	fmt.Println(icingaOut)
+	os.Exit(exitCode)
+}
+
+func writeNewStats(file string, stats Stats) error {
+	var content []byte
+
+	content, err := json.Marshal(stats)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	err = ioutil.WriteFile(file, content, 0644)
+	return err
+}
+
+func getOldStats(file string) (Stats, error) {
+	var stats Stats
+
+	content, _ := ioutil.ReadFile(file)
+	err := json.Unmarshal(content, &stats)
+
+	return stats, err
+}
